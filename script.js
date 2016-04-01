5bc141662cd5db4adcbfcafe8dde6013f8376632
Lennart Pelz <lennart.pelz@sinnerschrader.com>
2016-04-01 13:00:00 +0200

    Added handlers for getting previous, active and next slide on change

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
-	var activeSlide = function() {
-		return document.querySelector('.trigger:checked + .label + .slide');
-	},
-	previousSlide = function() {
-		return activeSlide().previousElementSibling.previousElementSibling.previousElementSibling;
-	},
-	nextSlide = function() {
-		return activeSlide().nextElementSibling.nextElementSibling.nextElementSibling;
-	};
+	var triggers			= document.querySelectorAll('.trigger'),
+		slides				= document.querySelectorAll('.slide'),
+		activeSlideIndex	= function(callback) {
+			triggers.forEach(function(element, index) {
+				element.checked && callback(index);
+			});
+		},
+		slideSet			= {},
+		getSlideSet			= function() {
+			activeSlideIndex(function(data) {
+				slideSet = {
+					prev:	slides[data-1] && slides[data-1] || false,
+					active:	slides[data] && slides[data] || false,
+					next:	slides[data+1] && slides[data+1] || false
+				};
+			});
+		}
+	;
+	triggers.forEach(function(element) {
+		element.addEventListener('change', function() {
+			getSlideSet();
+			console.log(slideSet);
+		});
+	});
+	getSlideSet();
+	console.log(slideSet);
 });
