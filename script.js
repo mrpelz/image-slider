c8610763dfbe61213c5ff3004eefaf4da1f22f01
Lennart Pelz <lennart.pelz@sinnerschrader.com>
2016-04-01 10:32:52 +0200

    Added fist touch handlers

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
-
+	var activeSlide = function() {
+		return document.querySelector('.trigger:checked + .label + .slide');
+	},
+	previousSlide = function() {
+		return activeSlide().previousElementSibling.previousElementSibling.previousElementSibling;
+	},
+	nextSlide = function() {
+		return activeSlide().nextElementSibling.nextElementSibling.nextElementSibling;
+	};
 });
