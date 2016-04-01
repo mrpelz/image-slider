9da847d23deedc718b0aa8e55b8d8639f173ad6c
Lennart Pelz <lennart.pelz@sinnerschrader.com>
2016-04-02 01:02:30 +0200

    Fixed dynamic width of slider_group

--- script.js
+++ script.js
@@ -1,68 +1,71 @@
 document.addEventListener('DOMContentLoaded', function () {
-	var sliderGroup				= document.querySelector('.slider_group'),
-		sliderGroupRect			= sliderGroup.getBoundingClientRect(),
-		sliderGroupWidth		= sliderGroup.offsetWidth,
-		triggers				= sliderGroup.querySelectorAll('.trigger'),
-		slides					= sliderGroup.querySelectorAll('.slide'),
-		activeTriggerIndex		= 0,
-		getActiveTriggerIndex	= function() {
+	var sliderGroup = document.querySelector('.slider_group'),
+		sliderGroupRect = sliderGroup.getBoundingClientRect(),
+		triggers = sliderGroup.querySelectorAll('.trigger'),
+		slides = sliderGroup.querySelectorAll('.slide'),
+		activeTriggerIndex = 0,
+		getActiveTriggerIndex = function() {
 			for(var index = 0; index < triggers.length; ++index) {
 				triggers[index].checked && (activeTriggerIndex = index);
 			}
 		},
-		triggerSet				= {},
-		getTriggerSet			= function() {
+		triggerSet = {},
+		getTriggerSet = function() {
 			triggerSet = {
 				p:		triggers[activeTriggerIndex-1] && triggers[activeTriggerIndex-1] || false,
 				a:		triggers[activeTriggerIndex] && triggers[activeTriggerIndex] || false,
 				n:		triggers[activeTriggerIndex+1] && triggers[activeTriggerIndex+1] || false
 			};
 		},
-		slideSet				= {},
-		getSlideSet				= function() {
+		slideSet = {},
+		getSlideSet = function() {
 			slideSet = {
 				p:		slides[activeTriggerIndex-1] && slides[activeTriggerIndex-1] || false,
 				a:		slides[activeTriggerIndex] && slides[activeTriggerIndex] || false,
 				n:		slides[activeTriggerIndex+1] && slides[activeTriggerIndex+1] || false
 			};
 		},
-		setTranslateX			= function(object, translateX) {
+		update = function() {
+			getActiveTriggerIndex();
+			getTriggerSet();
+			getSlideSet();
+		},
+		setTranslateX = function(object, translateX) {
 			if(object && translateX) {
 				object.style.transform = 'translate( ' + translateX + ' ,0)';
 			} else if(object && translateX === false) {
 				object.style.transform = '';
 			}
 		},
-		startLeftOffset			= 0,
-		lastLeftOffset			= 0,
-		movedPixels				= 0,
-		handleTouchStart		= function(event) {
+		startLeftOffset = 0,
+		lastLeftOffset = 0,
+		movedPixels = 0,
+		handleTouchStart = function(event) {
 			var leftOffset = event.touches[0] && (event.touches[0].clientX - sliderGroupRect.left) || false;
 			startLeftOffset = leftOffset;
 			lastLeftOffset = leftOffset;
 		},
-		handleTouchMove		= function(event) {
+		handleTouchMove = function(event) {
 			var leftOffset = event.touches[0] && (event.touches[0].clientX - sliderGroupRect.left) || false;
 			if(leftOffset !== lastLeftOffset) {
 				movedPixels = leftOffset - startLeftOffset;
 				slideSet.p && slideSet.p.classList.add('drag');
-				setTranslateX(slideSet.p, (movedPixels - sliderGroupWidth) + 'px');
+				setTranslateX(slideSet.p, 'calc(' + movedPixels + 'px - 100%)');
 				slideSet.a.classList.add('drag');
-				setTranslateX(slideSet.a, (movedPixels) + 'px');
+				setTranslateX(slideSet.a, movedPixels + 'px');
 				slideSet.n && slideSet.n.classList.add('drag');
-				setTranslateX(slideSet.n, (movedPixels + sliderGroupWidth) + 'px');
+				setTranslateX(slideSet.n, 'calc(' + movedPixels + 'px + 100%)');
 				lastLeftOffset = leftOffset;
 			}
 		},
-		handleTouchEnd			= function(event) {
+		handleTouchEnd = function(event) {
+			var sliderGroupWidth = sliderGroup.offsetWidth;
 			if(movedPixels < (sliderGroupWidth/2.5*-1)) {
 				triggerSet.n && (triggerSet.n.checked = true);
 			} else if(movedPixels > sliderGroupWidth/2.5) {
 				triggerSet.p && (triggerSet.p.checked = true);
 			}
-			getActiveTriggerIndex();
-			getTriggerSet();
-			getSlideSet();
+			update();
 			slideSet.p && slideSet.p.classList.remove('drag');
 			setTranslateX(slideSet.p, false);
 			slideSet.a.classList.remove('drag');
@@ -77,13 +80,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	for(var index = 0; index < triggers.length; ++index) {
 		triggers[index].addEventListener('change', function() {
-			getActiveTriggerIndex();
-			getTriggerSet();
-			getSlideSet();
+			update();
 		});
 	}
-	getTriggerSet();
-	getSlideSet();
+	update();
 
 	sliderGroup.addEventListener("touchstart", handleTouchStart, false);
 	sliderGroup.addEventListener("touchmove", handleTouchMove, false);
