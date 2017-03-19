cb425d7d3f11ae0c4c8923cd03da624136364b52
Nicolas Bettag <nicolas@darkcookies.de>
2017-03-19 22:39:02 +0100

    some file cleanup and remove outdated prefixes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
 	var slideThreshold = 2.5,
 		sliderGroups = document.querySelectorAll('.slider_group'),
 		run = function(sliderGroup) {
@@ -9,29 +9,29 @@ document.addEventListener('DOMContentLoaded', function () {
 				triggerSet = {},
 				slideSet = {},
 				update = function() {
-					for(var index = 0; index < triggers.length; ++index) {
+					for (var index = 0; index < triggers.length; ++index) {
 						triggers[index].checked && (activeTriggerIndex = index);
 					}
 					triggerSet = {
-						p: triggers[activeTriggerIndex-1],
+						p: triggers[activeTriggerIndex - 1],
 						a: triggers[activeTriggerIndex],
-						n: triggers[activeTriggerIndex+1]
+						n: triggers[activeTriggerIndex + 1]
 					};
 					slideSet = {
-						p: slides[activeTriggerIndex-1],
+						p: slides[activeTriggerIndex - 1],
 						a: slides[activeTriggerIndex],
-						n: slides[activeTriggerIndex+1]
+						n: slides[activeTriggerIndex + 1]
 					};
 				},
 				setTranslateX = function(object, translateX) {
-					if(object && translateX) {
+					if (object && translateX) {
 						object.style.transform = 'translate( ' + translateX + ' ,0)';
-					} else if(object && translateX === false) {
+					} else if (object && translateX === false) {
 						object.style.transform = '';
 					}
 				},
 				setDrag = function(offset) {
-					if(offset) {
+					if (offset) {
 						slideSet.p && slideSet.p.classList.add('drag');
 						setTranslateX(slideSet.p, 'calc(' + offset + 'px - 100%)');
 						slideSet.a.classList.add('drag');
@@ -58,25 +58,25 @@ document.addEventListener('DOMContentLoaded', function () {
 				handleTouchMove = function(event) {
 					var leftOffset = event.touches[0].clientX - sliderGroupRect.left,
 						sliderGroupWidth = sliderGroup.offsetWidth;
-					if(leftOffset !== lastLeftOffset) {
+					if (leftOffset !== lastLeftOffset) {
 						movedPixels = leftOffset - startLeftOffset;
-						if((slideSet.p && startLeftOffset < leftOffset) || (slideSet.n && startLeftOffset > leftOffset)) {
-							if(movedPixels < sliderGroupWidth && movedPixels > sliderGroupWidth*-1) {
+						if ((slideSet.p && startLeftOffset < leftOffset) || (slideSet.n && startLeftOffset > leftOffset)) {
+							if (movedPixels < sliderGroupWidth && movedPixels > sliderGroupWidth * -1) {
 								setDrag(movedPixels);
 								lastLeftOffset = leftOffset;
-							} else if(startLeftOffset < leftOffset) {
+							} else if (startLeftOffset < leftOffset) {
 								setDrag(sliderGroupWidth);
 							} else {
-								setDrag(sliderGroupWidth*-1);
+								setDrag(sliderGroupWidth * -1);
 							}
 						}
 					}
 				},
 				handleTouchEnd = function(event) {
 					var sliderGroupWidth = sliderGroup.offsetWidth;
-					if(movedPixels < sliderGroupWidth/slideThreshold*-1) {
+					if (movedPixels < sliderGroupWidth / slideThreshold * -1) {
 						triggerSet.n && (triggerSet.n.checked = true);
-					} else if(movedPixels > sliderGroupWidth/slideThreshold) {
+					} else if (movedPixels > sliderGroupWidth / slideThreshold) {
 						triggerSet.p && (triggerSet.p.checked = true);
 					}
 					update();
@@ -84,9 +84,8 @@ document.addEventListener('DOMContentLoaded', function () {
 					startLeftOffset = 0;
 					lastLeftOffset = 0;
 					movedPixels = 0;
-				}
-			;
-			for(var index = 0; index < triggers.length; ++index) {
+				};
+			for (var index = 0; index < triggers.length; ++index) {
 				triggers[index].addEventListener('change', function() {
 					update();
 				});
@@ -96,10 +95,9 @@ document.addEventListener('DOMContentLoaded', function () {
 			sliderGroup.addEventListener("touchmove", handleTouchMove, false);
 			sliderGroup.addEventListener("touchend", handleTouchEnd, false);
 			sliderGroup.addEventListener("touchcancel", handleTouchEnd, false);
-		}
-	;
-	if(sliderGroups) {
-		for(var index = 0; index < sliderGroups.length; ++index) {
+		};
+	if (sliderGroups) {
+		for (var index = 0; index < sliderGroups.length; ++index) {
 			run(sliderGroups[index]);
 		}
 	}
